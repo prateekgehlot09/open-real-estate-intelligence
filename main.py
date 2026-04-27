a0b24807c7c29fc9bba9a201a4a6768d7fea932b
Prateek Gehlot <prateek.gehlot09@gmail.com>
2026-04-27 18:51:21 +0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+from core.yield_validator import YieldValidator
+from core.risk_scoring import RiskScorer
+from models.report_generator import generate_report
+
+validator = YieldValidator(1500000, 105000)
+yield_value = validator.calculate_yield()
+
+scorer = RiskScorer(4, 4, 3)
+risk = scorer.risk_category()
+
+data = {
+    "location": "Business Bay",
+    "type": "1BR Apartment",
+    "price": 1500000,
+    "yield": yield_value,
+    "risk": risk,
+    "conclusion": "Income-stable asset"
+}
+
+print(generate_report(data))
