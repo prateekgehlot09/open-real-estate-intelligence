2b3f03befdee64627d2cfbb445a5892d69d2af57
Prateek Gehlot <prateek.gehlot09@gmail.com>
2026-04-27 19:17:13 +0400

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from models.report_generator import generate_report
 
 validator = YieldValidator(1500000, 105000)
 yield_value = validator.calculate_yield()
+gap = validator.projection_gap(9.5)
 
 scorer = RiskScorer(4, 4, 3)
 risk = scorer.risk_category()
